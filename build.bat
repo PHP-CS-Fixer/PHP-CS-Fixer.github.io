c439bdb80b91f2019cf462e4684d1d946ac0e8bb
Dariusz Ruminski <keradus@gmail.com>
2017-02-07 23:58:56 +0100

    update build script

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
+php download/php-cs-fixer-v2.phar self-update -q
+php download/php-cs-fixer-v2.phar readme > index.rst
+
 rm -rf build
 C:/Python35/Scripts/sphinx-build.exe . build
 touch build/.nojekyll
 rm -rf build/.doctrees build/_sources
+cp -r download build/download
 rm build/.buildinfo build/genindex.html build/objects.inv build/search.html build/searchindex.js
