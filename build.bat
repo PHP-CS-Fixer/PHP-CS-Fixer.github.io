3addfb4a237c0573717d32010bce17f2f3930856
Dariusz Ruminski <keradus@gmail.com>
2017-02-08 01:50:59 +0100

    add CNAME file

--- build.bat
+++ build.bat
@@ -6,5 +6,6 @@ rm -rf build
 C:/Python35/Scripts/sphinx-build.exe . build
 touch build/.nojekyll
 rm -rf build/.doctrees build/_sources build/theme
+cp CNAME build/CNAME
 cp -r download build/download
 rm build/.buildinfo build/genindex.html build/objects.inv build/search.html build/searchindex.js
